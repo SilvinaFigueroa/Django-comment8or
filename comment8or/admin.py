501a39cabbf496b2baa05e8839c40913d83ae7e6
SilvinaFigueroa <86253843+SilvinaFigueroa@users.noreply.github.com>
2023-05-14 18:51:36 -0700

    Upload project

--- comment8or/admin.py
+++ comment8or/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 class c8adminAdminSite(admin.AdminSite):
-    title_header = 'c8admin Admin'
-    site_header = 'c8 site admin'
+    title_header = 'c8 site admin'
+    site_header = 'c8admin'
     index_title = 'c8admin'
     logout_template = 'comment8or/logged_out.html'
